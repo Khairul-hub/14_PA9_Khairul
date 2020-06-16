1f3ac30ec25d638cd494e1db9ec262bbf4ce5cdc
Khairul-hub <64126807+Khairul-hub@users.noreply.github.com>
2020-06-16 12:42:19 +0800

    Currency Convertor project created by Khairul

---
+++ 14_Khairul_PA9/14_Khairul_PA9/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _14_Khairul_PA9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Convert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double amount = Convert.ToInt16(txt_Amount.Text);
+
+                if (amount <= 0)
+                {
+                    txt_Amount.Text = "INVALID NUMBER";
+                    return;
+                }
+
+                if (rb_US.Checked)
+                    amount *= 0.74;
+                else if (rb_Yen.Checked)
+                    amount *= 81.97;
+
+                txt_Converted.Text = Convert.ToString(amount);
+            }
+            catch (FormatException)
+            {
+                txt_Amount.Text = "NUMBERS ONLY";
+            }
+            catch (OverflowException)
+            {
+                txt_Amount.Text = "INVALID NUMBER";
+            }
+            
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            txt_Amount.Text = "";
+            txt_Converted.Text = "";
+        }
+
+        private void txt_Amount_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
